900b13f520ee4ac79d27d0c9563fc5469d1b049a
sobatha <natsuki.yokoizo.ny@gmail.com>
2023-10-20 23:20:28 +0900

    csrf

--- server.ts
+++ server.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import {router} from './routes/api'
+import {csrf} from './routes/csrf'
 
 const app: express.Express = express();
 const port: number = 3000;
 
 app.use(express.static("public"));
 app.use("/api", router)
+app.use("/csrf", csrf)
 app.get("/", (req, res, next)=>{
     res.end("Top Page")
 })
